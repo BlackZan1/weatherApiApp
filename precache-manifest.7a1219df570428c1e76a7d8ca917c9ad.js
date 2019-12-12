0c24013464230618ad9af5e26f8e3937690b6dfa
BlackZan1 <kenshin.nakamura.01@mail.ru>
2019-12-12 16:39:20 +0300

    Updates

---
+++ precache-manifest.7a1219df570428c1e76a7d8ca917c9ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c916f578d91318fa9db0814484cdfd",
+    "url": "/weatherApiApp/index.html"
+  },
+  {
+    "revision": "088089be91b49baa6239",
+    "url": "/weatherApiApp/static/css/main.a26703c4.chunk.css"
+  },
+  {
+    "revision": "fbd554a8cfc091e0f1bd",
+    "url": "/weatherApiApp/static/js/2.8f5cbae2.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/weatherApiApp/static/js/2.8f5cbae2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "088089be91b49baa6239",
+    "url": "/weatherApiApp/static/js/main.ec14d94a.chunk.js"
+  },
+  {
+    "revision": "de87572e9cfc85ace6b6",
+    "url": "/weatherApiApp/static/js/runtime-main.ae357589.js"
+  },
+  {
+    "revision": "e75cebddb8c77da2a0916ec120b36bb8",
+    "url": "/weatherApiApp/static/media/bg.e75cebdd.png"
+  },
+  {
+    "revision": "ae3ee640e986d698f7746b63a7b8484b",
+    "url": "/weatherApiApp/static/media/polygon.ae3ee640.jpg"
+  }
+]);
